3d2b45d4f9d111181fec746c8d4fb90b98bad406
Jason <development@pnxl.dev>
2024-01-09 17:36:38 +0700

    feat: wip update command

--- src/commands/legacy/admin/update.js
+++ src/commands/legacy/admin/update.js
@@ -8,9 +8,10 @@
  */
 
 // Import the required modules
-const { EmbedBuilder } = require("discord.js");
 const si = require("systeminformation");
-const package = require("../../../../package.json");
+const { execSync } = require("child_process");
+const { ButtonStyle, ButtonBuilder } = require("discord.js");
+const print = require("../../../helpers/print");
 
 // Export the command data for loader
 module.exports = {
@@ -55,5 +56,48 @@ module.exports = {
         "I couldn't update because my process wasn't ran with `pm2`. Try terminating and then starting me with `npm run start`."
       );
     }
+
+    const response = await message.reply("Checking for any new updates...");
+    execSync("git fetch", (error, stderr) => {
+      if (error) {
+        print.error(
+          `An error occured when trying to fetch the latest commit: ${error.message}`
+        );
+        return response
+          .edit(
+            `An error occured when trying to fetch the latest commit: ${error.message}`
+          )
+          .catch(print.error);
+      }
+      if (stderr) {
+        print.error(
+          `An error occured when trying to fetch the latest commit: ${stderr}`
+        );
+        return response
+          .edit(
+            `An error occured when trying to fetch the latest commit: ${error.message}`
+          )
+          .catch(print.error);
+      }
+      return;
+    });
+
+    const commitLocal = execSync("git rev-parse --short HEAD").slice(0, 7);
+
+    const commitRemote = execSync("git ls-remote").slice(0, 7);
+
+    if (commitLocal === commitRemote) {
+      return response
+        .edit(
+          `There are no updates! Rocky is running on the latest version. (\`${commitLocal}\`)`
+        )
+        .catch(print.error);
+    }
+
+    response.delete();
+
+    message.channel.send({
+      content: `There's a new update available! I am currently running on commit \`${commitLocal}\`\, but the newest commit is \`${commitRemote}\`. Would you like to update?`,
+    });
   },
 };
